fb5e81cc44f748117a38cf491c1d4970e6fab93c
wlsdks <wlsdks12@naver.com>
2023-12-30 14:35:36 +0900

    [blog-16] feat: 조금 더 심화된 http 서버 코드 추가 (여기서는 응답에서 한국어 에러가 발생함)

---
+++ src/main/java/com/study/blog/http/FullHttpServer.java
@@ -0,0 +1,74 @@
+package com.study.blog.http;
+
+import java.io.*;
+import java.net.*;
+import java.util.HashMap;
+import java.util.Map;
+
+public class FullHttpServer {
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(8080);
+        System.out.println("Listening for connection on port 8080 ....");
+
+        while (true) {
+            try (Socket clientSocket = serverSocket.accept()) {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+
+                // 여기서 먼저 null 처리를 해줘야함
+                String line = reader.readLine();
+                if (line == null) {
+                    continue; // 요청이 비어 있으면 다음 연결을 기다림
+                }
+
+                String[] requestLineParts = line.split(" ");
+                String httpMethod = requestLineParts[0];
+                String requestPath = requestLineParts[1];
+
+                Map<String, String> headers = new HashMap<>();
+                while ((line = reader.readLine()) != null) {
+                    if (line.isEmpty()) break;
+
+                    String[] header = line.split(": ");
+                    headers.put(header[0], header[1]);
+                }
+
+                // 헤더 정보 출력
+                System.out.println("HTTP Headers:");
+                for (Map.Entry<String, String> header : headers.entrySet()) {
+                    System.out.println(header.getKey() + ": " + header.getValue());
+                }
+
+                String responseBody = "";
+                if ("GET".equals(httpMethod)) {
+                    responseBody = handleGetRequest(requestPath);
+                } else if ("POST".equals(httpMethod)) {
+                    responseBody = handlePostOrPutRequest(reader, headers);
+                } else if ("PUT".equals(httpMethod)) {
+                    responseBody = handlePostOrPutRequest(reader, headers);
+                }
+
+                String httpResponse = "HTTP/1.1 200 OK\r\nContent-Length: " + responseBody.length() + "\r\n\r\n" + responseBody;
+                writer.write(httpResponse);
+                writer.flush();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private static String handleGetRequest(String path) {
+        // GET 요청 처리 로직
+        return "GET 요청에 대한 응답: " + path;
+    }
+
+    private static String handlePostOrPutRequest(BufferedReader reader, Map<String, String> headers) throws IOException {
+        // POST와 PUT 요청 본문 처리 로직
+        int contentLength = Integer.parseInt(headers.getOrDefault("Content-Length", "0"));
+        StringBuilder requestBody = new StringBuilder();
+        for (int i = 0; i < contentLength; i++) {
+            requestBody.append((char) reader.read());
+        }
+        return "Received data: " + requestBody.toString();
+    }
+}
