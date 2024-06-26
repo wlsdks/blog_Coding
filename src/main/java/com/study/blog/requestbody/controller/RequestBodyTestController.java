4e00622571842a8e78dd159055ddd99ed1eaadbd
wlsdks <wlsdks12@naver.com>
2024-06-26 22:39:19 +0900

    feat: 테스트를 위해 GetMapping도 추가

--- src/main/java/com/study/blog/requestbody/controller/RequestBodyTestController.java
+++ src/main/java/com/study/blog/requestbody/controller/RequestBodyTestController.java
@@ -2,6 +2,7 @@ package com.study.blog.requestbody.controller;
 
 import com.study.blog.requestbody.dto.TestDto;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,11 @@ public class RequestBodyTestController {
 
     @PostMapping("/requestbody")
     public ResponseEntity<TestDto> requestBody(@RequestBody TestDto dto) {
+        return ResponseEntity.ok(dto);
+    }
+
+    @GetMapping("/requestbody")
+    public ResponseEntity<TestDto> requestBodyGet(TestDto dto) {
         return ResponseEntity.ok(dto);
     }
 
