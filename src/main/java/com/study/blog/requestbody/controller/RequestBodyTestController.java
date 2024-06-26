399252aeb58ab98893fa9cb2b93430c42d34d061
wlsdks <wlsdks12@naver.com>
2024-06-27 07:04:41 +0900

    feat: 로그 추가

--- src/main/java/com/study/blog/requestbody/controller/RequestBodyTestController.java
+++ src/main/java/com/study/blog/requestbody/controller/RequestBodyTestController.java
@@ -1,6 +1,7 @@
 package com.study.blog.requestbody.controller;
 
 import com.study.blog.requestbody.dto.TestDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,15 +9,18 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class RequestBodyTestController {
 
     @PostMapping("/requestbody")
     public ResponseEntity<TestDto> requestBody(@RequestBody TestDto dto) {
+        log.info("dto = {}", dto);
         return ResponseEntity.ok(dto);
     }
 
     @GetMapping("/requestbody")
     public ResponseEntity<TestDto> requestBodyGet(TestDto dto) {
+        log.info("dto = {}", dto);
         return ResponseEntity.ok(dto);
     }
 
