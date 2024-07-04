311a5fe5d01e70330381e45560686744e16991d5
wlsdks <wlsdks12@naver.com>
2024-07-04 23:06:56 +0900

    feat: 응답 전용 어노테이션 추가

--- src/main/java/com/study/blog/requestbody/controller/RequestBodyTestController.java
+++ src/main/java/com/study/blog/requestbody/controller/RequestBodyTestController.java
@@ -2,22 +2,22 @@ package com.study.blog.requestbody.controller;
 
 import com.study.blog.requestbody.dto.TestDto;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @Slf4j
 public class RequestBodyTestController {
 
+    @ResponseStatus(HttpStatus.ACCEPTED)
     @PostMapping("/requestbody")
     public ResponseEntity<TestDto> requestBody(@RequestBody TestDto dto) {
         log.info("dto = {}", dto);
         return ResponseEntity.ok(dto);
     }
 
+    @ResponseStatus(HttpStatus.ACCEPTED)
     @GetMapping("/requestbody")
     public ResponseEntity<TestDto> requestBodyGet(TestDto dto) {
         log.info("dto = {}", dto);
